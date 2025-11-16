f0e237697d572221655d750e427098ab8e0aedd2
nikita-0209 <59296031+nikita-0209@users.noreply.github.com>
2025-11-16 08:50:10 +0000

    Deploying to gh-pages from @ nikita-0209/nikita-0209.github.io@f8ff7b1d23bea89271e47ffcb990d71531e20953 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,33 +16,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/assets/pdf/Nikita_Resume_2025.pdf";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
-        },{id: "nav-submenus",
-          title: "submenus",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/_pages/dropdown/";
           },
         },{id: "post-a-post-with-image-galleries",
       
